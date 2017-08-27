1ed2edad46bca9df140688ce1f3a44f9aa6dbf04
LauraPrichici <p_laura_c@yahoo.com>
2017-08-26 22:23:22 -0700

    Added UIColor extension

--- Homework/Extensions.swift
+++ Homework/Extensions.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Laura. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension String {
     func convertDate(to newPattern: String) -> String {
@@ -22,5 +22,36 @@ extension String {
             returnDate = formatter.string(from: date)
         }
         return returnDate ?? ""
+    }
+}
+
+extension UIColor {
+    
+    static let textViewColor = UIColor.init(fromHexString: "#c7c7cdff")
+    
+    public convenience init?(fromHexString: String) {
+        
+        let r, g, b, a: CGFloat
+        
+        if fromHexString.hasPrefix("#") {
+            let start = fromHexString.characters.index(fromHexString.startIndex, offsetBy: 1)
+            let hexColor = fromHexString.substring(from: start)
+            
+            if hexColor.characters.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8 ) / 255
+                    a = CGFloat((hexNumber & 0x000000ff)      ) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        return nil
     }
 }
